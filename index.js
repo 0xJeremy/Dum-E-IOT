9b84acce209fc6fa86d98d64cdab97a88fe25102
0xJeremy <jeremy.kanovsky@tufts.edu>
2019-02-07 22:40:24 -0500

    Fixed Webserver Endpoints

--- index.js
+++ index.js
@@ -20,23 +20,25 @@ app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 app.use('/', express.static(__dirname + '/public'));
 
-var action = ""
+var action_jarvis = "";
+var action_friday = "";
+var action_virgil = "";
 
 app.get('/', function(req, res) {
-	res.render('index.html');
+    res.render('index.html');
 });
 
 app.post('/jarvis', function(req, res){
     console.log('POST /');
     console.dir(req.body);
-    action = req.body.value;
+    action_jarvis = req.body.value;
     res.writeHead(200, {'Content-Type': 'text/html'});
 });
 
 app.post('/friday', function(req, res){
     console.log('POST /');
     console.dir(req.body);
-    action = req.body.value;
+    action_friday = req.body.value;
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('thanks');
 });
@@ -44,19 +46,19 @@ app.post('/friday', function(req, res){
 app.post('/virgil', function(req, res){
     console.log('POST /');
     console.dir(req.body);
-    action = req.body.value;
+    action_virgil = req.body.value;
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('thanks');
 });
 
 app.get('/get_jarvis', function(req, res) {
-	res.send(action)
+    res.send(action_jarvis)
 });
 app.get('/get_friday', function(req, res) {
-	res.send(action)
+    res.send(action_friday)
 });
 app.get('/get_virgil', function(req, res) {
-	res.send(action)
+    res.send(action_virgil)
 });
 
 app.listen(PORT, function() { console.log("Listening on port " + PORT)});
