a1d3de21db87818495f2b1411635f3fccac29cd4
0xJeremy <jeremy.kanovsky@tufts.edu>
2019-03-09 20:59:32 -0500

    Added more robots

--- index.js
+++ index.js
@@ -20,21 +20,26 @@ app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 app.use('/', express.static(__dirname + '/public'));
 
-var action_jarvis = "";
-var action_friday = "";
-var action_virgil = "";
+var action_jarvis  = "";
+var action_friday  = "";
+var action_virgil  = "";
+var action_homer   = "";
+var action_plato   = "";
+var action_jokasta = "";
+var action_helen   = "";
+
 
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
+// Endpoint POSTing ---------------------------------------------
 app.post('/jarvis', function(req, res){
     console.log('POST /');
     console.dir(req.body);
     action_jarvis = req.body.value;
     res.writeHead(200, {'Content-Type': 'text/html'});
 });
-
 app.post('/friday', function(req, res){
     console.log('POST /');
     console.dir(req.body);
@@ -42,7 +47,6 @@ app.post('/friday', function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('thanks');
 });
-
 app.post('/virgil', function(req, res){
     console.log('POST /');
     console.dir(req.body);
@@ -50,7 +54,38 @@ app.post('/virgil', function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('thanks');
 });
+app.post('/homer', function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+    action_homer = req.body.value;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('thanks');
+});
+app.post('/plato', function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+    action_plato = req.body.value;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('thanks');
+});
+app.post('/jokasta', function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+    action_jokasta = req.body.value;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('thanks');
+});
+app.post('/helen', function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+    action_helen = req.body.value;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('thanks');
+});
+//---------------------------------------------------------------
 
+
+// Endpoint GETting ---------------------------------------------
 app.get('/get_jarvis', function(req, res) {
 	res.send(action_jarvis)
 });
@@ -60,5 +95,18 @@ app.get('/get_friday', function(req, res) {
 app.get('/get_virgil', function(req, res) {
 	res.send(action_virgil)
 });
+app.get('/get_homer', function(req, res) {
+    res.send(action_homer)
+});
+app.get('/get_plato', function(req, res) {
+    res.send(action_plato)
+});
+app.get('/get_jokasta', function(req, res) {
+    res.send(action_jokasta)
+});
+app.get('/get_helen', function(req, res) {
+    res.send(action_helen)
+});
+//---------------------------------------------------------------
 
 app.listen(PORT, function() { console.log("Listening on port " + PORT)});
--- public/index.js
+++ public/index.js
@@ -22,13 +22,27 @@ var jarvis = document.getElementById("jarvis");
 jarvis.onclick = function() {
 	selected_robot = "jarvis";
 }
-
 var friday = document.getElementById("friday");
 friday.onclick = function() {
 	selected_robot = "friday";
 }
-
 var virgil = document.getElementById("virgil");
 virgil.onclick = function() {
 	selected_robot = "virgil";
+}
+var homer = document.getElementById("homer");
+virgil.onclick = function() {
+	selected_robot = "homer";
+}
+var plato = document.getElementById("plato");
+virgil.onclick = function() {
+	selected_robot = "plato";
+}
+var jokasta = document.getElementById("jokasta");
+virgil.onclick = function() {
+	selected_robot = "jokasta";
+}
+var helen = document.getElementById("helen");
+virgil.onclick = function() {
+	selected_robot = "helen";
 }
